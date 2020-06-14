79984fa45563cb417a61c1b54cd372ed826936f0
mrnenz <61102537+mrnenz@users.noreply.github.com>
2020-06-14 16:21:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,8 +9,11 @@ self.addEventListener('install', function(event) {
           '/src/js/app.js',
           '/manifest.json',
           '/favicon.ico',
+          '/src/icons/icon-72x72.png',
           '/src/icons/icon-96x96.png',
+          '/src/icons/icon-128x128.png',
           '/src/icons/icon-144x144.png',
+          '/src/icons/icon-152x152.png',
           '/src/icons/icon-192x192.png',
           '/src/icons/icon-384x384.png',
           '/src/icons/icon-512x512.png'
